872dc1a9fbf4f7ccc0b031564b0e67f634e0a169
Flysmallfish <2040244628@qq.com>
2023-03-18 09:28:39 +0800

    666

--- features.py
+++ features.py
@@ -13,9 +13,18 @@ class kugouMusicBot():
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
 
         topJson = requests.get(
+            "http://m.kugou.com/rank/info/?rankid=8888&page=1&json=true", headers=head).text
+        jsonDict = json.loads(topJson)
+        print(json.dumps(jsonDict['info']))
+
+    def top_list(self):
+        head = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'}
+
+        topJson = requests.get(
             "http://m.kugou.com/rank/list&json=true", headers=head).text
-        jsonDict=json.loads(topJson)
-        
+        jsonDict = json.loads(topJson)
+        print(jsonDict)
 
 
 bot = kugouMusicBot()
